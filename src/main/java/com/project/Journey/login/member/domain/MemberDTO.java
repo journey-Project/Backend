51df87ec579a3064cd1e4d15390243a12d7c8712
Kim Seungwon <sprauncy76@gmail.com>
2024-12-06 00:10:10 +0900

    fix: MemberDTO 유효성 검증 메세지 추가 및 수정
      - "ID는 필수 값입니다"로 ID 검증 메시지 추가
      - "이름은 필수 값입니다", "비밀번호는 필수 값입니다" 메시지 추가
      - "올바른 이메일 형식이어야 합니다"로 이메일 검증 메시지 수정

--- src/main/java/com/project/Journey/login/member/domain/MemberDTO.java
+++ src/main/java/com/project/Journey/login/member/domain/MemberDTO.java
@@ -7,15 +7,17 @@ import lombok.Data;
 @Data
 public class MemberDTO {
 
-    @NotBlank
+    @NotBlank(message = "ID는 필수 값입니다")
     private String id;
 
-    @NotBlank
+    @NotBlank(message = "이름은 필수 값입니다")
     private String name;
-    @NotBlank
+
+    @NotBlank(message = "비밀번호는 필수 값입니다")
     private String password;
-    @NotBlank
-    @Email
+
+    @NotBlank(message = "이메일은 필수 값입니다")
+    @Email(message = "올바른 이메일 형식이어야 합니다")
     private String email;
 
     // Oauth2 최초 로그인 후 회원가입 시 필요한 정보
