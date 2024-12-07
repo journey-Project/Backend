9cadf4705381168f82b047ec9bb4cfaef351e9b8
Kim Seungwon <sprauncy76@gmail.com>
2024-12-07 23:13:27 +0900

    test: OAuth2 유틸리티 및 사용자 정보 클래스 테스트 추가
    OAuth2 관련 유틸리티와 사용자 정보 클래스를 테스트하기 위한 단위 테스트를 추가
    - OAuth2Utils의 SocialType 및 OAuth2UserInfo 파싱 테스트 추가
    - Google, Kakao, Naver의 OAuth2UserInfo 클래스 각각에 대한 파싱 테스트 작성
    - OAuth2Attributes의 유효성 검증 및 GoogleOAuth2UserInfo와의 연계 테스트 작성

---
+++ src/test/java/com/project/Journey/login/oauth2/domain/OAuth2AttributesTest.java
@@ -0,0 +1,31 @@
+package com.project.Journey.login.oauth2.domain;
+
+import com.project.Journey.login.member.domain.SocialType;
+import com.project.Journey.login.oauth2.userInfo.GoogleOAuth2UserInfo;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class OAuth2AttributesTest {
+
+    @Test
+    void testOauth2AttributesOf() {
+        // Given
+        Map<String, Object> attributes = new HashMap<>();
+
+        attributes.put("sub", "12345");
+        attributes.put("email", "test@example.com");
+        attributes.put("name", "Test User");
+
+        // When
+        OAuth2Attributes oauth2Attributes = OAuth2Attributes.of(SocialType.GOOGLE, "sub", attributes);
+
+        // Then
+        assertThat(oauth2Attributes.getNameAttributeKey()).isEqualTo("sub");
+        assertThat(oauth2Attributes.getOauth2UserInfo()).isInstanceOf(GoogleOAuth2UserInfo.class);
+    }
+
+}
