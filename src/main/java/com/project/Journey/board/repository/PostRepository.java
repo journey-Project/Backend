d6a05a67ec1a6f5dfdc3a78d4a47dd2eb19b4d17
jkchoi490 <jkchoi490@daum.net>
2024-12-23 17:41:08 +0900

    feat: 게시글 조회수 증가를 위한 업데이트 쿼리 추가

--- src/main/java/com/project/Journey/board/repository/PostRepository.java
+++ src/main/java/com/project/Journey/board/repository/PostRepository.java
@@ -2,14 +2,22 @@ package com.project.Journey.board.repository;
 
 
 import com.project.Journey.board.entity.Post;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
+    @Transactional
+    @Modifying
+    @Query("UPDATE Post p SET p.view_count = :increment WHERE p.post_id = :post_id")
+    void incrementViewCount(@Param("post_id") Long postId, @Param("increment") int increment);
+
 
 
 }
