725e2152a958daaae4a2d27149bd24069ef604c8
Kim Seungwon <sprauncy76@gmail.com>
2024-12-30 10:13:38 +0900

    feat: 댓글 리포지토리 구현

---
+++ src/main/java/com/project/Journey/board/comment/repository/CommentRepository.java
@@ -0,0 +1,16 @@
+package com.project.Journey.board.comment.repository;
+
+
+import com.project.Journey.board.comment.domain.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+    // 특정 post_id에 대한 댓글 목록 조회
+    List<Comment> findByPost_Post_id(Long postId);
+
+
+
+}
