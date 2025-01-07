0cc4b21bd05697e5d2dd9d3916ae19a5c0628d5b
jkchoi490 <jkchoi490@daum.net>
2025-01-07 23:57:49 +0900

    fix: whitelist에 swagger 관련 url 추가

--- src/main/java/com/project/Journey/login/security/config/WebSecurityConfig.java
+++ src/main/java/com/project/Journey/login/security/config/WebSecurityConfig.java
@@ -32,7 +32,7 @@ public class WebSecurityConfig {
     private static final String[] SWAGGER_WHITELIST = {"/", "/login", "/loginHome", "/signUp", "/renew", "/loginSuccess",
             "/login/oauth2/code/**", "/oauth2/signUp", "/error", "/js/**", // Swagger UI & Docs
             "/swagger-ui", "/v3/api-docs", "/swagger-ui/index.html", "/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs/**"
-            ,"/api/**", "/WebSocketTest.html","/ws/**"};
+            ,"/api/**", "/WebSocketTest.html","/ws/**","/swagger-ui"};
 
 
     @Bean
