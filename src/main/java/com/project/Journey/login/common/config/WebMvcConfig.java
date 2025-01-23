8ddb82112cf0318447096844fc5978d39369eaac
Kim Seungwon <sprauncy76@gmail.com>
2025-01-23 16:52:17 +0900

    fix: CORS 설정 추가

--- src/main/java/com/project/Journey/login/common/config/WebMvcConfig.java
+++ src/main/java/com/project/Journey/login/common/config/WebMvcConfig.java
@@ -2,6 +2,7 @@ package com.project.Journey.login.common.config;
 
 import com.project.Journey.login.common.interceptor.AccessPermissionCheckInterceptor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -21,5 +22,14 @@ public class WebMvcConfig implements WebMvcConfigurer {
         registry.addInterceptor(new AccessPermissionCheckInterceptor())
                 .order(1)
                 .excludePathPatterns("/**"); // 모든 요청 허용 (테스트용)
+    }
+
+
+    // CORS 설정 추가
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // 모든 경로에 대해 CORS 설정
+                .allowedOrigins("*") // 모든 도메인 허용
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS"); // 허용할 HTTP 메서드
     }
 }
