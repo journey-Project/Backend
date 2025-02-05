f982aa026c8b137d47f3fc7bd9241932fd69b3eb
Kim Seungwon <sprauncy76@gmail.com>
2025-02-05 17:39:55 +0900

    feat: 회원가입 페이지 인증 권한 수정
    - 회원가입용 URL을 로그인 없이도 접속 가능하도록 변경

--- src/main/java/com/project/Journey/login/security/config/WebSecurityConfig.java
+++ src/main/java/com/project/Journey/login/security/config/WebSecurityConfig.java
@@ -35,7 +35,7 @@ public class WebSecurityConfig {
     private static final String[] WHITELIST = {"/", "/login", "/loginHome", "/signUp", "/renew", "/loginSuccess",
             "/login/oauth2/code/**", "/oauth2/signUp", "/error", "/js/**", // Swagger UI & Docs
             "/swagger-ui", "/v3/api-docs", "/swagger-ui/index.html", "/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs/**"
-            ,"/api/**", "/WebSocketTest.html","/ws/**","/swagger-ui"};
+            ,"/api/**", "/WebSocketTest.html","/ws/**","/swagger-ui", "/api/auth/sign-up", "/api/oauth2/sign-up"};
 
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
