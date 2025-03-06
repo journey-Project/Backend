f9abeca35b01c42ca3c3b443eccce2d334f5b087
Kim Seungwon <sprauncy76@gmail.com>
2025-03-06 15:35:51 +0900

    fix: CORS 설정 및 인터셉터 예외 처리 수정

--- src/main/java/com/project/Journey/login/common/config/WebMvcConfig.java
+++ src/main/java/com/project/Journey/login/common/config/WebMvcConfig.java
@@ -1,10 +1,10 @@
-package com.project.Journey.login.common.config;
+    package com.project.Journey.login.common.config;
 
-import com.project.Journey.login.common.interceptor.AccessPermissionCheckInterceptor;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+    import com.project.Journey.login.common.interceptor.AccessPermissionCheckInterceptor;
+    import org.springframework.context.annotation.Configuration;
+    import org.springframework.web.servlet.config.annotation.CorsRegistry;
+    import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+    import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
@@ -21,15 +21,20 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new AccessPermissionCheckInterceptor())
                 .order(1)
-                .excludePathPatterns("/**"); // 모든 요청 허용 (테스트용)
+                .excludePathPatterns("/", "/login", "/loginHome", "/signUp", "/renew", "/loginSuccess",
+                        "/login/oauth2/code/**", "/oauth2/signUp", "/error", "/js/**", // Swagger UI & Docs
+                        "/swagger-ui", "/v3/api-docs", "/swagger-ui/index.html", "/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs/**"
+                        ,"/api/**", "/WebSocketTest.html","/ws/**","/swagger-ui", "/api/auth/sign-up", "/api/oauth2/sign-up", "/oauth2/**"); // 필요한 경로만 예외처리
     }
+
 
 
     // CORS 설정 추가
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 모든 경로에 대해 CORS 설정
-                .allowedOrigins("*") // 모든 도메인 허용
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS"); // 허용할 HTTP 메서드
+                .allowedOrigins("http://localhost:5173", "https://dxkiwmo9p9ise.cloudfront.net") // 프론트엔드 도메인만 허용
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP 메서드
+                .allowCredentials(true); // 쿠키 허용
     }
 }
