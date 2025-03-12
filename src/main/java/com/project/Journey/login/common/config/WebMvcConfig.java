34015620bb2171dbec9a285108c1dbf07696707d
Kim Seungwon <sprauncy76@gmail.com>
2025-03-12 15:27:59 +0900

    feat: CORS 설정에 와일드카드 도메인 추가
    - allowedOriginPatterns에 "https://*.journeysite.site" 추가

--- src/main/java/com/project/Journey/login/common/config/WebMvcConfig.java
+++ src/main/java/com/project/Journey/login/common/config/WebMvcConfig.java
@@ -9,14 +9,6 @@
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
-//    @Override
-//    public void addInterceptors(InterceptorRegistry registry) {
-//        registry.addInterceptor(new AccessPermissionCheckInterceptor())
-//                .order(1)
-//                .excludePathPatterns("/", "/login", "/loginHome", "/signUp", "/renew", "/loginSuccess",
-//                        "/login/oauth2/code/**", "/oauth2/signUp", "/error", "/js/**", "/swagger-ui", "/v3/api-docs","/swagger-ui/index.html", "/swagger-ui/**", "/v3/api-docs/**", "/swagger-ui.html");
-//    }
-
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new AccessPermissionCheckInterceptor())
@@ -27,14 +19,13 @@ public class WebMvcConfig implements WebMvcConfigurer {
                         ,"/api/**", "/WebSocketTest.html","/ws/**","/swagger-ui", "/api/auth/sign-up", "/api/oauth2/sign-up", "/oauth2/**"); // 필요한 경로만 예외처리
     }
 
-
-
     // CORS 설정 추가
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**") // 모든 경로에 대해 CORS 설정
-                .allowedOrigins("http://localhost:5173", "https://dxkiwmo9p9ise.cloudfront.net") // 프론트엔드 도메인만 허용
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP 메서드
-                .allowCredentials(true); // 쿠키 허용
+        registry.addMapping("/**")
+                .allowedOriginPatterns("https://*.journeysite.site") // 와일드카드
+                .allowedOrigins("http://localhost:5173", "https://dxkiwmo9p9ise.cloudfront.net")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowCredentials(true);
     }
 }
