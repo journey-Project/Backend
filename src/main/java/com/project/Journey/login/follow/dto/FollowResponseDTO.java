5eaa1682620f6fd5e2e9c75e05d82e866c8ed549
jkchoi490 <jkchoi490@daum.net>
2025-05-25 23:40:57 +0900

    docs : 팔로우(팔로잉) 기능 API 명세서 작성(#47)

--- src/main/java/com/project/Journey/login/follow/dto/FollowResponseDTO.java
+++ src/main/java/com/project/Journey/login/follow/dto/FollowResponseDTO.java
@@ -1,6 +1,8 @@
 package com.project.Journey.login.follow.dto;
 
 import com.project.Journey.login.member.domain.Member;
+
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,9 +10,13 @@ import lombok.Setter;
 @Setter
 public class FollowResponseDTO {
 
+    @Schema(description = "회원의 고유 ID", example = "1")
     private Long memberId;
+
+    @Schema(description = "회원의 로그인 ID", example = "user1")
     private String username;
 
+    @Schema(description = "회원의 프로필 이미지 URL", example = "https://example.com/images/profile1.png")
     private String profileImageUrl;
 
 
