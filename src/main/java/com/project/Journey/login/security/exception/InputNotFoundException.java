407f2a24024fd3841afd81047af62b39b4f1ef42
AshtonSW <sprauncy76@gmail.com>
2025-06-15 16:12:46 +0900

    chore: 불필요한 클래스 및 미사용 코드 정리
    - 사용되지 않는 import, 메서드, 필드 제거
    - 더 이상 참조되지 않는 클래스 삭제
    - 리팩터링 과정에서 남은 dead code 제거

--- src/main/java/com/project/Journey/login/security/exception/InputNotFoundException.java
+++
@@ -1,9 +0,0 @@
-//package com.project.Journey.login.security.exception;
-//
-//import org.springframework.security.core.AuthenticationException;
-//
-//public class InputNotFoundException extends AuthenticationException {
-//    public InputNotFoundException(String m) {
-//        super(m);
-//    }
-//}
