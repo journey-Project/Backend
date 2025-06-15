8c4744ad3397a8d435e858ce4cce520062b9e381
AshtonSW <sprauncy76@gmail.com>
2025-06-15 16:15:10 +0900

    feat: 전역 시간대를 Asia/Seoul로 설정 (#69)
    - application.properties에 spring.jackson.time-zone=Asia/Seoul 추가
    - @PostConstruct를 통해 JVM 전역 시간대(TimeZone) 설정
    - 한국 시간 기준의 로그 및 날짜 데이터 일관성 확보

--- src/main/java/com/project/Journey/JourneyApplication.java
+++ src/main/java/com/project/Journey/JourneyApplication.java
@@ -1,10 +1,13 @@
 package com.project.Journey;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import java.util.TimeZone;
 
 @EnableJpaAuditing
 @EnableScheduling
@@ -15,5 +18,8 @@ public class JourneyApplication {
 		SpringApplication.run(JourneyApplication.class, args);
 	}
 
-
+	@PostConstruct
+	public void started() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 }
